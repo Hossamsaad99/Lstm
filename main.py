29047ffe3f9ba6086e59fbbdc55fd94a03cfade1
Hossam Saad <62431581+Hossamsaad99@users.noreply.github.com>
2021-05-12 07:41:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -17,8 +17,65 @@ import uvicorn
 import pydantic
 import gunicorn
 import httptools
-
+from tensorflow import keras
+from keras.models import load_model
 app = FastAPI()
+
+def lstm(data_set):
+  """
+  Getting the desired data from yahoo, then doing some data manipulation such as data
+  reshaping
+  Args:
+      (str) data_set - the ticker of desired dataset (company)
+  Returns:
+      (float) diff_prediction - the model out-put (the prediction of the next day)
+      (float) real_prediction - the model output + today's price (real price of tomorrow)
+  """
+
+  # data gathering
+  df = pdr.DataReader(data_set, data_source='yahoo', start=date.today() - timedelta(100))
+
+  # data manipulation
+
+  # creating a new df with Xt - Xt-1 values of the close prices (most recent 60 days)
+  close_df = df['2012-01-01':].reset_index()['Close'][-61:]
+  close_diff = close_df.diff().dropna()
+  data = np.array(close_diff).reshape(-1, 1)
+
+  # reshaping the data to 3D to be accepted by our LSTM model
+  model_input = np.reshape(data, (1, 60, 1))
+
+  # loading the model and predicting
+  loaded_model = load_model("lstm_f_60.hdf5")
+  model_prediction = float(loaded_model.predict(model_input))
+  real_prediction = model_prediction + df['Close'][-1]
+  
+
+  return model_prediction, real_prediction
+
+def Regression(ticker):
+  """
+  Forcasting using an ensambled model between SVR, Ridge and Linear regression! by Getting the desired data from yahoo, 
+  then doing some data manipulation
+  Args:
+      (str) ticket - the ticker of desired dataset (company)
+  Returns:
+      (float) arima_output - the model out-put (the prediction of the next day)
+      (float) diff - the model output - today's price (the diff between tomorrow's prediction and today's real value)
+  """
+  start_date = datetime.now() - timedelta(1)
+  start_date = datetime.strftime(start_date, '%Y-%m-%d')
+
+  df = pdr.DataReader(ticker, data_source='yahoo', start=start_date)  # read data
+  df.drop('Volume', axis='columns', inplace=True)
+  X = df[['High', 'Low', 'Open', 'Adj Close']]  # input columns
+  y = df[['Close']]  # output column
+  input = X
+  loaded_model = pickle.load(open('regression_model.pkl', 'rb'))
+  reg_prediction = loaded_model.predict(input)
+  reg_diff=reg_prediction-df.Close[-1]
+
+  return  reg_prediction,reg_diff
 
 def Arima(ticker):
     
@@ -49,9 +106,15 @@ def index():
 @app.post('/predict')
 
 async def predict_price(ticker:str):
-    
-    fc ,diff = Arima(ticker)
-    return {'prediction':fc[0],'difference':diff[0]}
+    if data == 'F':
+      
+      arima_prediction, diff = arima(data)
+      model_prediction, lstm_prediction = lstm(data)
+      reg_prediction,reg_diff = Regression(data)
+  
+    return {'Arima prediction' : arima_prediction[0],'LSTM prediction' : lstm_prediction,regression prediction' : reg_prediction[0]}
+        
+        '
        
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
