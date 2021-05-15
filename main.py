2185b2c17fcb900bc4e93afd42a4519e57405a44
Hossam Saad <62431581+Hossamsaad99@users.noreply.github.com>
2021-05-15 15:50:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def predict_price(ticker:str):
   
     return {'LSTM prediction' : lstm_prediction}
         
-        '
+      
        
 if __name__ == '__main__':
     uvicorn.run(app, host='127.0.0.1', port=8000)
